3342afb346bf8e7fa5182f66230e8fafac09a61e
RachelGcp <rysy3692@gmail.com>
2025-11-02 14:30:57 +0200

    add price property

--- ShoppingAPI/ShoppingAPI/Models/Category.cs
+++ ShoppingAPI/ShoppingAPI/Models/Category.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty; 
         public decimal DiscountPercentage { get; set; }
+        public decimal Price {get;set;}
         public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
